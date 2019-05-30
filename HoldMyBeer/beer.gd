b9252fb9a161eebb3d670ed3035bfff190f55c5c
Hermitifier <Quantifier666@gmail.com>
2019-05-30 23:19:24 +0200

    Beer glass - add simple score

--- HoldMyBeer/beer.gd
+++ HoldMyBeer/beer.gd
@@ -3,23 +3,28 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var lost = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"GUI/Label".text = str(lost)
+	$"GUI/Label".text = str(beer_globals.score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_Area2D_body_entered(body):
-	lost += 1
-	$"GUI/Label".text = str(lost)
+	beer_globals.lost += 1
+	beer_globals.current -= 1;
+	#$"GUI/Label".text = str(lost)
 	body.queue_free()
-	if lost == beer_globals.total:
+	#if lost == beer_globals.total:
+	if beer_globals.current <= 0:
+		#trigger gameover
 		#get_tree().change_scene_to()
 		#get_parent().wyswietl_game_over()
 		get_tree().reload_current_scene()
 		pass
 
+func _process(delta):
+	beer_globals.score += delta*beer_globals.current
+	$"GUI/Label".text = str(beer_globals.score)
--- HoldMyBeer/beer_globals.gd
+++ HoldMyBeer/beer_globals.gd
@@ -2,6 +2,8 @@ extends Node
 
 var total = 144
 var current = 0
+var lost = 0
+var score = 0.0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
