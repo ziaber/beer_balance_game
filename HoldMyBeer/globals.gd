d235d8d7f7ae5b4f020f998add15e418dad9a7e8
Damian <damian.trybala@gmail.com>
2019-05-31 00:17:30 +0200

    fix after merge

--- HoldMyBeer/globals.gd
+++ HoldMyBeer/globals.gd
@@ -3,12 +3,9 @@ extends Node
 var speed  = 200
 var max_speed = 200
 
-<<<<<<< HEAD
+
 var score = 0
 var total = 144
 var current = 0
 var lost = 0
 var glass_rotation = 0.0
-=======
-var score = 0
->>>>>>> 14b0accdbd96cebd46e0337ec306aa25600a45b7
